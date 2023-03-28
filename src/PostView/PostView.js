e23532d167d0393db53698c5430dd906b19b4ae6
VIKRAM SINGH <66213553+vikramsaharan72056@users.noreply.github.com>
2023-03-28 21:56:41 +0530

    address updated to server online address

--- src/PostView/PostView.js
+++ src/PostView/PostView.js
@@ -16,7 +16,7 @@ const style2 = {
 
 
 useEffect(() =>{
-  fetch("http://localhost:8080/allpost").then((res) =>res.json()).then((result) => setData(result))
+  fetch("https://serverme-rsmp.onrender.com/allpost").then((res) =>res.json()).then((result) => setData(result))
 })
 
   return (
