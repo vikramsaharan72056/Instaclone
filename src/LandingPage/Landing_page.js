204321ac63462acf78a4bec15e61eec761d81e9a
vikramsaharan72056 <vikramsaharan72056@gmail.com>
2023-04-06 16:03:28 +0530

    node modules deleted

--- src/LandingPage/Landing_page.js
+++ src/LandingPage/Landing_page.js
@@ -1,83 +1,86 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import "../App.css";
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const LogIn = () => {
-    const [showpass,setShowPass] = useState(false)
-    const [inputVal,setInputVal] = useState({
-        email:"",
-        password:""
+    const [showpass, setShowPass] = useState(false)
+    const [inputVal, setInputVal] = useState({
+        email: "",
+        password: ""
     })
     const defaultPage = useNavigate();
-   
-    
-    const LogInUser = async (e) =>{
+
+
+    const LogInUser = async (e) => {
         e.preventDefault();
-        const {email,password} = inputVal;
-        
-         if(email ===""){
+        const { email, password } = inputVal;
+
+        if (email === "") {
             alert("please enter your email")
-          }else if(!email.includes("@")){
+        } else if (!email.includes("@")) {
             alert("please enter correct email")
-          }else if(password ===""){
+        } else if (password === "") {
             alert("please enter your password")
-          }else if (password.length<6){
+        } else if (password.length < 6) {
             alert("password must be atleast of 6 characters")
-          }else{
-            
-            const data =  await fetch("https://serverme-rsmp.onrender.com/login",{
-                method:"Post",
-                headers:{
-                    "Content-Type":"application/json"
+        } else {
+
+            const data = await fetch("https://serverme-rsmp.onrender.com/login", {
+                method: "Post",
+                headers: {
+                    "Content-Type": "application/json"
                 },
-                body:JSON.stringify({
-                    email,password
+                body: JSON.stringify({
+                    email, password
                 })
             });
             const res = await data.json();
-    
-             if(res.status === 201){
+
+            if (res.status === 201) {
                 defaultPage("/allpost")
-                localStorage.setItem("usercokie",res.result.token)
+                localStorage.setItem("usercokie", res.result.token)
                 alert("user logged in successfully")
-                setInputVal({...inputVal,email:"",password:""})
-             }else if(res.status === 403){
-                 alert("password not matched")
-                 setInputVal({...inputVal,email:"",password:""})
-          }else if(res.status === 422){
-              alert("fill all the details")
-              setInputVal({...inputVal,email:"",password:""})
+                setInputVal({ ...inputVal, email: "", password: "" })
+            } else if (res.status === 403) {
+                alert("password not matched")
+                setInputVal({ ...inputVal, email: "", password: "" })
+            } else if (res.status === 422) {
+                alert("fill all the details")
+                setInputVal({ ...inputVal, email: "", password: "" })
+            }
+        }
     }
     
-    return (
-        <section>
-            <div className='form_data'>
-                <div className='form_heading'>
-                    <h1>Welcome Back, Log In</h1>
-                    <p>We are glad that you are back ,Please login </p>
-                </div>
-                <form>
-                    <div className='form_input'>
-                        <label htmlFor='mail'>Email</label>
-                        <input type="email" id="lmail" onChange = {(e) =>setInputVal({...inputVal,email:e.target.value})} value = {inputVal.email}name='email' placeholder='enter your email address' />
+
+        return (
+            <section>
+                <div className='form_data'>
+                    <div className='form_heading'>
+                        <h1>Welcome Back, Log In</h1>
+                        <p>We are glad that you are back ,Please login </p>
                     </div>
-                    <div className='form_input'>
-                        <label htmlFor='pass'>Password</label>
-                        <div className='two'>
-                            <input type={!showpass ?"password":"text"} onChange = {(e) =>setInputVal({...inputVal,password:e.target.value})} value = {inputVal.password}id="lpass" name='pass' placeholder='enter your password' />
-                            <div className='showpass' onClick = {() =>setShowPass(!showpass)}>
-                                {!showpass ?"Show":"Hide"}</div>
-                            
+                    <form>
+                        <div className='form_input'>
+                            <label htmlFor='mail'>Email</label>
+                            <input type="email" id="lmail" onChange={(e) => setInputVal({ ...inputVal, email: e.target.value })} value={inputVal.email} name='email' placeholder='enter your email address' />
                         </div>
+                        <div className='form_input'>
+                            <label htmlFor='pass'>Password</label>
+                            <div className='two'>
+                                <input type={!showpass ? "password" : "text"} onChange={(e) => setInputVal({ ...inputVal, password: e.target.value })} value={inputVal.password} id="lpass" name='pass' placeholder='enter your password' />
+                                <div className='showpass' onClick={() => setShowPass(!showpass)}>
+                                    {!showpass ? "Show" : "Hide"}</div>
 
-                    </div>
-                    <button className='btn' onClick = {LogInUser}>Login</button>
-                    <p>You don't have an account? then, <NavLink to = "/register">SignUp</NavLink></p>
-                </form>
-            </div>
-        </section>
-    )
-}
+                            </div>
 
-export default LogIn
+                        </div>
+                        <button className='btn' onClick={LogInUser}>Login</button>
+                        <p>You don't have an account? then, <NavLink to="/register">SignUp</NavLink></p>
+                    </form>
+                </div>
+            </section>
+        )
+    }
+
+    export default LogIn
 
