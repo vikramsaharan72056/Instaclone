53bdc94060308780f1d2a7f42237063fedfe78e8
vikramsaharan72056 <vikramsaharan72056@gmail.com>
2023-04-09 19:49:51 +0530

    post change

--- src/PostView/PostView.js
+++ src/PostView/PostView.js
@@ -1,41 +1,49 @@
-import React,{useEffect,useState} from 'react'
-
+import React, { useEffect, useState } from "react";
 
 const PostView = () => {
-const [data,setData] = useState([]);
-const style1 = {
-  border: "1px solid black",
-  width:"90%",
-  margin:"5%",
-}
-const style2 = {
-  border:"1px solid black",
-  height:"50px"
-}
+  const [data, setData] = useState([]);
+  const [like, setLikes] = useState(data.likes);
+  const style1 = {
+    border: "1px solid black",
+    width: "90%",
+    margin: "5%",
+  };
+  const style2 = { border: "1px solid black", height: "50px" };
 
+  useEffect(() => {
+    fetch("https://serverme-rsmp.onrender.com/allpost")
+      .then((res) => res.json())
+      .then((result) => setData(result));
+  });
 
-useEffect(() =>{
-  fetch("https://serverme-rsmp.onrender.com/allpost").then((res) =>res.json()).then((result) => setData(result))
-})
-
-  return (
-     data.map ( item =>{
-      return(
+  return data.map((item) => {
+    return (
       <div style={style1}>
-        <div style = {style2}>
-        <h3 style = {{"margin":"1px"}}>{item.name}</h3> 
-     <span style = {{"margin":"1px"}}> {item.location}</span>
+        <div style={style2}>
+          <h3 style={{ margin: "1px" }}>{item.name}</h3>
+          <span style={{ margin: "1px" }}> {item.location}</span>
         </div>
-        <div><img src={item.image} alt="" height="33%" width="100%" />
-
+        <div>
+          <img src={item.image} alt="" height="15%" width="100%" />
         </div>
-      <h4>{item.description}</h4>
-      
-     
-       </div>)
-     })
-    
-  )
-    }
+        <div>
+          <h5>{item.description}</h5>
+        </div>
+        <div>
+          <h5>
+            <img
+              onClick={(like) => setLikes(like + 1)}
+              src="/like-icon-png-4158.png"
+              height="2%"
+              width="2%"
+              alt="liker"
+            />
+            <span style={{ height: "15%", width: "15%" }}>{item.likes}</span>
+          </h5>
+        </div>
+      </div>
+    );
+  });
+};
 
-export default PostView
+export default PostView;
